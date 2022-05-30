9d9e2729963fa33b43a516517663888610749f9b
Max <maksim23558@gmail.com>
2022-05-30 22:54:40 +0300

    added categories, added black/white themes

--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -1,7 +1,6 @@
 export 'detail_screen.dart';
 export 'main_screen.dart';
-export 'news_screen.dart';
+export 'home_screen.dart';
 export 'profile_screen.dart';
-export 'search_screen.dart';
 export 'sign_in.dart';
 export 'sign_up.dart';
