46feaffbd9848ff880d2ae18c7bdbfd9fd4a8613
Max <maksim23558@gmail.com>
2022-06-04 21:02:48 +0300

    added country change in news

--- lib/src/screens/detail_screen.dart
+++ lib/src/screens/detail_screen.dart
@@ -1,7 +1,7 @@
-import 'package:cached_network_image/cached_network_image.dart';
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app/src/models/models.dart';
-import 'package:intl/intl.dart';
 
 class DetailScreen extends StatelessWidget {
   final Articles news;
@@ -9,7 +9,6 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    DateTime date = news.pubDate;
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -41,19 +40,6 @@ class DetailScreen extends StatelessWidget {
                 ),
               ),
             ),
-            // Positioned(
-            //   top: 360,
-            //   child: Padding(
-            //     padding: const EdgeInsets.only(left: 20.0),
-            //     child: Text(
-            //       date == null
-            //           ? ''
-            //           : DateFormat.yMd().add_jm().format(date).toString(),
-            //       style: const TextStyle(
-            //           fontSize: 16, fontWeight: FontWeight.w600),
-            //     ),
-            //   ),
-            // ),
             Positioned(
               top: 350,
               child: SizedBox(
@@ -64,7 +50,7 @@ class DetailScreen extends StatelessWidget {
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(
-                            left: 20.0, right: 20.0, top: 30.0),
+                            left: 20.0, right: 20.0, top: 10.0),
                         child: Text(
                           news.title,
                           style: const TextStyle(
@@ -74,49 +60,102 @@ class DetailScreen extends StatelessWidget {
                         ),
                       ),
                       news.category == null
-                          ? Container()
-                          : SingleChildScrollView(
-                              child: Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 10.0, left: 20.0),
-                                child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Container(
-                                    width: 80,
-                                    height: 35,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.black12,
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(10.0),
-                                      ),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(10),
-                                      child: ListView.builder(
-                                        scrollDirection: Axis.horizontal,
-                                        itemBuilder: ((context, index) =>
-                                            Center(
-                                              child: Text(
-                                                news.category[index],
-                                              ),
-                                            )),
-                                        itemCount: 1,
-                                        shrinkWrap: true,
-                                      ),
-                                    ),
+                          ? const Divider()
+                          : Padding(
+                              padding: const EdgeInsets.only(left: 20.0),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: SizedBox(
+                                  width: 200,
+                                  height: 25,
+                                  child: ListView.builder(
+                                    scrollDirection: Axis.horizontal,
+                                    itemBuilder: ((context, index) => Center(
+                                          child: Text(
+                                            '#${news.category[index]}'
+                                                .toUpperCase(),
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                        )),
+                                    itemCount: 1,
+                                    shrinkWrap: true,
                                   ),
                                 ),
                               ),
                             ),
                       Padding(
-                        padding: const EdgeInsets.only(
-                            left: 20.0, right: 20.0, top: 10.0, bottom: 50.0),
-                        child: Text(
-                          news.content == null ? '' : news.content,
-                          style: const TextStyle(
-                              fontSize: 17, fontWeight: FontWeight.w400),
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Row(
+                            children: [
+                              news.creator == null
+                                  ? const Divider()
+                                  : Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 10.0),
+                                      child: SizedBox(
+                                        width: 200,
+                                        height: 30,
+                                        child: ListView.builder(
+                                          scrollDirection: Axis.horizontal,
+                                          itemBuilder: ((context, index) => Row(
+                                                children: [
+                                                  const Text(
+                                                    'Creator:',
+                                                    style: TextStyle(
+                                                        fontSize: 18,
+                                                        fontWeight:
+                                                            FontWeight.w500),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 3,
+                                                  ),
+                                                  Text(
+                                                    news.creator[index],
+                                                    style: const TextStyle(
+                                                        fontSize: 18,
+                                                        fontWeight:
+                                                            FontWeight.w400),
+                                                  ),
+                                                ],
+                                              )),
+                                          itemCount: 1,
+                                          shrinkWrap: true,
+                                        ),
+                                      ),
+                                    ),
+                            ],
+                          ),
                         ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20.0, right: 20.0, bottom: 50.0),
+                        child: news.content == null
+                            ? Column(
+                                children: [
+                                  const Text(
+                                    'Error! This post has no description!',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Back to main screen'))
+                                ],
+                              )
+                            : Text(
+                                news.content,
+                                style: const TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.w400),
+                              ),
+                      ),
+                      const Divider(),
                     ],
                   ),
                 ),
